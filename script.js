f32e15c2d8a02a99933a2f5b20543b4624adcc3f
@sushant0911 <sushantgupta.brg2003@gmail.com>
2024-09-29 16:36:24 +0530

    added course page section

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-const searchIcon = document.getElementById('search-icon');
-const searchBar = document.getElementById('search-bar');
-const logo = document.getElementById('logo');
-
-searchIcon.addEventListener('click', function() {
-  if (window.innerWidth <= 567) { 
-    searchBar.style.width = "100%";
-    logo.style.display = "none";
-    searchBar.style.backgroundColor = "#ffffff";
-    searchBar.style.display = "block";
-    searchBar.focus();
-  }
-});
-
-// Handle clicking outside the search bar to collapse and restore the logo
-document.addEventListener('click', function(e) {
-  if (window.innerWidth <= 579) { 
-    if (e.target !== searchIcon && e.target !== searchBar) {
-      logo.style.display = "block";
-      // searchBar.style.display = "block";
-      searchBar.style.width = "10px";
-      searchBar.style.backgroundColor = "transparent";
-      // searchIcon.style.display = "block";
-    }
-  }  
-});
-
 
 const searchCourses = () => {
   const searchValue = document.getElementById('course-search').value.toLowerCase();
